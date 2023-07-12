b9e9b0b7b42519dee3919dd88cc9fa70737cc8ec
Vysakhdn <vysakhdn123@gmail.com>
2023-07-12 17:01:49 +0530

    Qtn23

--- Qtn23.js
+++ Qtn23.js
@@ -2,12 +2,11 @@ let s=[[1,2,3],[4,5,6],[7,8,9]];
 var p1=0;
 var p2=0;
 var d;
-var q;
-var c;
 for(let i=0;i<s.length;i++){
-    for(let j=0;j<i;j++){
-        if(i==j){
-            q=p1+s[i][j];
-        }
-    }
+    p1=p1+s[i][i];
+    d=s[i].length-1-i;
+    p2=p2+s[i][d];
 }
+console.log(p1);
+console.log(p2);
+console.log(p1-p2);
