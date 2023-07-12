7b30107698bd61d6fd257871d4f24d29e699d94e
Vysakhdn <vysakhdn123@gmail.com>
2023-07-12 16:38:38 +0530

    Qtn18

--- Qtn18.js
+++ Qtn18.js
@@ -1,6 +1,16 @@
-let s="I'm learning javascript basics";
-for(let i=0;i<s.length;i++){
-    if(s[i]==" " || s[i]=="."){
-        console.log(s[i]);
+let s="I'm learning javascript basics.";
+var c=[];
+var b='';
+var i=0;
+while(i<s.length)
+    {
+    b=b+s[i];
+    if(s[i]==" " || s[i]==".")
+    {
+
+    c.push(b);
+    b='';
     }
-}
+    i++;
+    }
+console.log(c);
---
+++ Qtn23.js
@@ -0,0 +1,13 @@
+let s=[[1,2,3],[4,5,6],[7,8,9]];
+var p1=0;
+var p2=0;
+var d;
+var q;
+var c;
+for(let i=0;i<s.length;i++){
+    for(let j=0;j<i;j++){
+        if(i==j){
+            q=p1+s[i][j];
+        }
+    }
+}
