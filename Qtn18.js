b1d7da28acab2ce72f671167949f61f244e76a52
Vysakhdn <vysakhdn123@gmail.com>
2023-07-12 14:11:29 +0530

    Qtn19

---
+++ Qtn18.js
@@ -0,0 +1,6 @@
+let s="I'm learning javascript basics";
+for(let i=0;i<s.length;i++){
+    if(s[i]==" " || s[i]=="."){
+        console.log(s[i]);
+    }
+}
---
+++ Qtn19.js
@@ -0,0 +1,8 @@
+let s= "Hi Iam Tony";
+let c=0;
+for(let i=0;i<s.length;i++){
+    if(s[i]==" " || s[i]=="."){
+        c++;
+    }   
+}
+console.log(c); 
