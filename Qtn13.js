ed037116d521e96b74f9f96d1dee4d0474d588e2
Vysakhdn <vysakhdn123@gmail.com>
2023-07-12 12:24:57 +0530

    change in Qtn13;

--- Qtn13.js
+++ Qtn13.js
@@ -1,4 +1,6 @@
 let s="This is something boring";
 for(let i=0;i<s.length;i++){
-    if(s[i]==)
+    if((s[i]!="a")&&(s[i]!="A")&&(s[i]!="e")&&(s[i]!="E")&&(s[i]!="i")&&(s[i]!="I")&&(s[i]!="o")&&(s[i]!="O")&&(s[i]!="u")&&(s[i]!="U")){
+        console.log(s[i]);
+    }
 }
